1d6c6073fb31ceea7db06a343762e37c067fe04f
Hasan Tayyar BESIK <tayyar.besik@gmail.com>
2016-07-30 00:13:25 +0300

    every conection has a random color

--- index.js
+++ index.js
@@ -8,8 +8,18 @@ app.get('/', function(req, res) {
 });
 
 io.on('connection', function(socket) {
+  var color = (function() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  })();
+
   socket.on('_message', function(msg) {
-    io.emit('_message', msg);
+    msg = words.replace(msg);
+    io.emit('_message', msg + "###" + color);
   });
 });
 
